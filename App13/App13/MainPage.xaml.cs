a953f4f10083abc7232802a25d39adf3e5ad3c05
Elsayed Mohamed <elsayed201@hotmail.com>
2017-03-26 20:47:56 +0300

    Add project files.

---
+++ App13/App13/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App13
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Mainlistview.ItemsSource = new List<string>
+            {
+"elsayed","mohamed","elmetwally","mohamed","احمد","علي ","عثمان"
+            };
+        }
+
+
+
+
+
+    
+    }
+}
